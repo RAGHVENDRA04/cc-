49492e7d3735d1246c2fcecbe8bb9f5b5a893c3f
Raghvendra Singh <108226988+RAGHVENDRA04@users.noreply.github.com>
2024-11-15 09:31:56 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -12,7 +12,7 @@ export const handleError = (msg) => {
     })
 }
 
-export const APIUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+export const APIUrl = process.env.REACT_APP_API_URL || 'https://cc-jrorxoo18-raghvendra-singhs-projects-d6b9570c.vercel.app';
 
 
 
